6a86ee6c92470f769e2c7d204e09eb15dff897f7
ZhouRuicong <18823547629@163.com>
2018-10-29 17:45:25 +0800

    添加了图片上传功能

--- script/chat.js
+++ script/chat.js
@@ -52,6 +52,7 @@ CongChat.prototype={
 					let div=document.createElement('div'),
 						img=document.createElement('img');
 					div.classList.add('emojiContainerDiv');
+					img.draggable=false;
 					img.src="./images/chat-tool/emoji/"+item;
 					img.title=item.split('.')[0];
 					div.appendChild(img);
@@ -85,7 +86,7 @@ CongChat.prototype={
 				li.innerHTML=`<div class="message others">
 									<div class="sender-photo">
 										<div>
-											<img src="./images/亚索.png" />
+											<img src="./images/亚索.png" draggable="false" />
 										</div>
 									</div>
 									<div class="sender-other">
@@ -102,12 +103,12 @@ CongChat.prototype={
 				li.innerHTML=`<div class="message others">
 									<div class="sender-photo">
 										<div>
-											<img src="./images/亚索.png" />
+											<img src="./images/亚索.png" draggable="false" />
 										</div>
 									</div>
 									<div class="sender-other">
 										<div class="sender-name">${nickname}</div>
-										<img src="${imgSrc}"/>
+										<img src="${imgSrc}" draggable="false"/>
 									</div>
 								</div>`;
 				document.getElementById('messageList').appendChild(li);
@@ -115,10 +116,7 @@ CongChat.prototype={
 			});
 		});
 		
-		document.getElementById('showEmojiArea').addEventListener('click',function(){//显示表情包
-			let emojiArea=document.getElementById('emojiArea');
-			emojiArea.classList.toggle('emoji-area-show');
-		});
+		
 		document.getElementById('joinChat').addEventListener('click',function(){//注册
 			let nickname=document.getElementById('nicknameInput').value.trim();
 			if(nickname!=""){
@@ -141,45 +139,39 @@ CongChat.prototype={
 		document.getElementById('emojiArea').addEventListener('click',function(event){//发送表情包
 			let e=event||window.event;
 			let target=e.target;
-
 			if(target.tagName.toLowerCase()=='img'){
-				that.socket.emit('sendEmoji',target.src);
-				let li=document.createElement('div');
-				li.innerHTML=`<div class="message your">								
-									<div class="sender-other">
-										<div class="sender-name">${that.nickname}</div>
-										<img src="${target.src}"/>
-									</div>
-									<div class="sender-photo">
-										<div>
-											<img src="./images/亚索2.png" />
-										</div>
-									</div>
-					</div>`;
-				document.getElementById('messageList').appendChild(li);
-				document.getElementById('emojiArea').classList.remove('emoji-area-show');
-				that.scrollToDown(document.getElementById('messageShowContainer'));
+				that.sendEmoji(that.nickname,target.src);
 			}
 			else if (target.tagName.toLowerCase()=='div'&&target.classList.contains('emojiContainerDiv')){
-				that.socket.emit('sendEmoji',target.getElementsByTagName('img')[0].src);
-				let li=document.createElement('div');
-				li.innerHTML=`<div class="message your">								
-									<div class="sender-other">
-										<div class="sender-name">${that.nickname}</div>
-										<img src="${target.getElementsByTagName('img')[0].src}"/>
-									</div>
-									<div class="sender-photo">
-										<div>
-											<img src="./images/亚索2.png" />
-										</div>
-									</div>
-					</div>`;
-				document.getElementById('messageList').appendChild(li);
-				document.getElementById('emojiArea').classList.remove('emoji-area-show');
-				that.scrollToDown(document.getElementById('messageShowContainer'));
+				that.sendEmoji(that.nickname,target.getElementsByTagName('img')[0].src);		
 			}
 			else{
 
+			}
+		});
+
+
+		//工具栏
+		document.getElementById('showEmojiArea').addEventListener('click',function(){//显示表情包
+			let emojiArea=document.getElementById('emojiArea');
+			emojiArea.classList.toggle('emoji-area-show');
+		});
+		document.getElementById('uploadPhotoButton').addEventListener('click',function(){//选择发送图片
+			document.getElementById('realUploadPhoto').click();
+
+		});
+		document.getElementById('realUploadPhoto').addEventListener('change',function(){//发送图片
+			if(this.files.length!=0){
+				let file=this.files[0],
+					reader=new FileReader();
+				if(!reader){
+					return;
+				}
+				reader.onload=function(e){
+					this.value='';
+					that.sendEmoji(that.nickname,e.target.result);	
+				};
+				reader.readAsDataURL(file);	
 			}
 		});
 	},
@@ -188,7 +180,7 @@ CongChat.prototype={
 			htmlStr="";
 		users.forEach(function(item,index){
 			htmlStr=htmlStr+`<li>
-								<img src="./images/timg.jpg"  height="50px">
+								<img src="./images/timg.jpg" draggable="false"  height="50px">
 								<span>${item}</span>
 							</li>`;
 		});
@@ -198,10 +190,28 @@ CongChat.prototype={
 	scrollToDown(dom){
 		dom.scrollTop+=dom.scrollHeight;	
 	},
+	sendEmoji(nickname,src){
+			this.socket.emit('sendEmoji',src);
+			let li=document.createElement('div');
+			li.innerHTML=`<div class="message your">								
+								<div class="sender-other">
+									<div class="sender-name">${nickname}</div>
+									<img src="${src}" draggable="false" />
+								</div>
+								<div class="sender-photo">
+									<div>
+										<img src="./images/亚索2.png" draggable="false" />
+									</div>
+								</div>
+					</div>`;
+			document.getElementById('messageList').appendChild(li);
+			document.getElementById('emojiArea').classList.remove('emoji-area-show');
+			this.scrollToDown(document.getElementById('messageShowContainer'));
+	},
 	sendMessage(){//发送消息
-		let message=document.getElementById('sendMessageContent').value.trim();
+		let message=document.getElementById('sendMessageContent').innerHTML.trim();
 			if(message!=''){
-				document.getElementById('sendMessageContent').value="";
+				document.getElementById('sendMessageContent').innerHTML="";
 				let li=document.createElement('div');
 				this.socket.emit('sendMessage',message);
 				li.innerHTML=`<div class="message your">								
@@ -211,14 +221,13 @@ CongChat.prototype={
 									</div>
 									<div class="sender-photo">
 										<div>
-											<img src="./images/亚索2.png" />
+											<img src="./images/亚索2.png" draggable="false" />
 										</div>
 									</div>
 					</div>`;
 				document.getElementById('messageList').appendChild(li);
 				document.getElementById('sendMessageContent').focus();
-				this.scrollToDown(document.getElementById('messageShowContainer'));
-				
+				this.scrollToDown(document.getElementById('messageShowContainer'));		
 			}
 			else{
 				alert('消息发送不能为空');
